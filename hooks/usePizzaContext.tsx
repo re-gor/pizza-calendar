1887386b246498c35bd5f299144b74d5dadd3951
Egor Bykhovtsev <bykhovtsev@yandex-team.ru>
2022-01-07 14:24:41 +0300

    prepare gh-pages

--- hooks/usePizzaContext.tsx
+++
@@ -1,4 +0,0 @@
-import {useContext} from 'react';
-import {PizzaContext} from 'components/PizzaContextProvider';
-
-export const usePizzaContext = () => useContext(PizzaContext);
--- utils/types.ts
+++
@@ -1,3 +0,0 @@
-export type ReturnType<T> = T extends (...args: any[]) => infer R ? R : any;
-export type ActionCreator<Type extends string, Payload> = (payload: Payload) => {type: Type, payload: Payload};
-export type EmptyActionCreator<Type extends string> = () => {type: Type};
