7e224c57396ff266b2be9cd8193dd0363c36c079
Egor Bykhovtsev <bykhovtsev@yandex-team.ru>
2022-01-07 14:35:40 +0300

    prepare gh-pages

--- precache-manifest.378d5118078305865703e768e565b8b7.js
+++ precache-manifest.378d5118078305865703e768e565b8b7.js
@@ -1,46 +1,46 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e2288214f9c7c035229755fe85dec3cb",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "006b6a845bc2846f05e6",
-    "url": "/static/css/2.6a58fec0.chunk.css"
+    "url": "./static/css/2.6a58fec0.chunk.css"
   },
   {
     "revision": "e13f1f43870e144fbdd8",
-    "url": "/static/css/main.77db60b8.chunk.css"
+    "url": "./static/css/main.77db60b8.chunk.css"
   },
   {
     "revision": "006b6a845bc2846f05e6",
-    "url": "/static/js/2.53eccc67.chunk.js"
+    "url": "./static/js/2.53eccc67.chunk.js"
   },
   {
     "revision": "e13f1f43870e144fbdd8",
-    "url": "/static/js/main.41532fe6.chunk.js"
+    "url": "./static/js/main.41532fe6.chunk.js"
   },
   {
     "revision": "116b09fc44efd44dee9f",
-    "url": "/static/js/runtime~main.775709df.js"
+    "url": "./static/js/runtime~main.775709df.js"
   },
   {
     "revision": "7acb5c1590b70d766d84ec50a5bca271",
-    "url": "/static/media/pizza_detailed.7acb5c15.svg"
+    "url": "./static/media/pizza_detailed.7acb5c15.svg"
   },
   {
     "revision": "feac705b2aaedb8b3dd67f8350dce18e",
-    "url": "/static/media/pizza_detailed_mono.feac705b.svg"
+    "url": "./static/media/pizza_detailed_mono.feac705b.svg"
   },
   {
     "revision": "c4a46534813318faee2ffb16c349c558",
-    "url": "/static/media/pizza_heart.c4a46534.svg"
+    "url": "./static/media/pizza_heart.c4a46534.svg"
   },
   {
     "revision": "416ef2327a5bf33dc745c2eecd4fa369",
-    "url": "/static/media/pizza_slice.416ef232.svg"
+    "url": "./static/media/pizza_slice.416ef232.svg"
   },
   {
     "revision": "2e280982fd53df502f15d0be877629cb",
-    "url": "/static/media/pizza_slice_mono.2e280982.svg"
+    "url": "./static/media/pizza_slice_mono.2e280982.svg"
   }
 ]);
